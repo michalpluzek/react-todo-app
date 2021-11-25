f86502b80cf895e35e6a618b9573a0214c741e0d
Michał Płużek <michalpluzek@gmail.com>
2021-11-25 17:23:04 +0100

    Refabric task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,32 +4,16 @@ import "./TaskList.css";
 
 const TaskList = (props) => {
   const activeTasks = props.tasks
-    .map((task) => {
-      if (!task.isActive) return null;
-      return (
-        <Task
-          key={task.id}
-          task={task}
-          remove={props.remove}
-          done={props.done}
-        />
-      );
-    })
-    .filter((task) => task);
+    .filter((task) => task.isActive)
+    .map((task) => (
+      <Task key={task.id} task={task} remove={props.remove} done={props.done} />
+    ));
 
   const doneTasks = props.tasks
-    .map((task) => {
-      if (task.isActive) return null;
-      return (
-        <Task
-          key={task.id}
-          task={task}
-          remove={props.remove}
-          done={props.done}
-        />
-      );
-    })
-    .filter((task) => task);
+    .filter((task) => !task.isActive)
+    .map((task) => (
+      <Task key={task.id} task={task} remove={props.remove} done={props.done} />
+    ));
 
   return (
     <>
