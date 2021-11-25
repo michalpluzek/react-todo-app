884e005fef31b4e30e49d961e497d36ab5263dfe
Michał Płużek <michalpluzek@gmail.com>
2021-11-25 18:29:18 +0100

    Add template and style to tasklist

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -19,12 +19,17 @@ const TaskList = (props) => {
     <>
       <div className="activeTasks">
         <h2>{`Zadania do zrobienia (${activeTasks.length})`}</h2>
-        {activeTasks}
+        {activeTasks.length > 0 ? activeTasks : <p>Brak zadań. Super!</p>}
       </div>
       <hr />
       <div className="doneTasks">
         <h3>{`Zadania do zrobienia (${doneTasks.length})`}</h3>
-        {doneTasks}
+        {doneTasks.length > 5 && (
+          <span style={{ fontSize: 10 }}>
+            Wyświetlonych jest tylko 5 ostatnio ukończonych zadań
+          </span>
+        )}
+        {doneTasks.slice(0, 5)}
       </div>
     </>
   );
